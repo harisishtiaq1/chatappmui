3049ef368f3fc5064d1fc145d1cb1ad0a42ac7c6
harisishtiaq1 <harisharry232@gmail.com>
2023-05-24 11:28:08 +0500

    updated

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import SignUp from "../Signup/Signup";
+import SignIn from "../../Signin/Signin";
 
 function Layout() {
-  return <Navbar />;
+  return <SignIn/>;
 }
 
 export default Layout;
