060ed8466989bd79684ac10e9d3e80cb86f38d63
harisishtiaq1 <harisharry232@gmail.com>
2023-05-26 14:52:02 +0500

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage} from "firebase/storage";
 import { getFirestore } from "@firebase/firestore";
-
+import { getMessaging } from "firebase/messaging";
 const firebaseConfig = {
   apiKey: "AIzaSyDe9Qy_mCUu2qsV5EBR8-6w7ULIY-StyjA",
   authDomain: "chatapp-ffa2d.firebaseapp.com",
@@ -17,3 +17,4 @@ export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const storage = getStorage()
+export const messaging = getMessaging(app);
